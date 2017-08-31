c8f949d74765875a24df43dab7c1f2488a82d864
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-08-31 20:15:09 +0200

    remove console logs

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     $('#searchBox').on('keyup', function(e) {
         let searchVal = e.target.value;
-        console.log(searchVal);
-        // make request to Wikipedia API
         $.ajax({
             url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + searchVal,
             // data: queryData,
@@ -12,10 +10,10 @@ $(document).ready(function() {
         }).done(function(data){
             $('#results').empty();
             let results = data.query.search;
-            console.log(results);
             for(let i = 0; i < results.length; i++) {
-                const url = `https://en.wikipedia.org/wiki/${results[i].title.replace(/ /g, "_")}`;
-                let html =
+                // const url = `https://en.wikipedia.org/wiki/${results[i].title.replace(/ /g, "_")}`;
+                const url = 'https://en.wikipedia.org/?curid=' + results[i].pageid;
+                const html =
                 `<div class="card">
                   <a href="${url}" target="_blank">
                     <div class="card-body">
@@ -25,7 +23,6 @@ $(document).ready(function() {
                   </a>
                 </div>
                 <br>`;
-
                 $('#results').append(html);
             }
         });
